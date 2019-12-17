a12e3c4b750900a6452bb26f41f75e3f136d8fad
Lorena Mitrea <mitrea.lorenageo@gmail.com>
2019-12-17 15:09:25 +0200

    Added files and changes required for deployment to heroku

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LocalLibraryL.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LocalLibrary.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
