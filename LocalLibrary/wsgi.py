7d2ace6606d129fb939e8a994ab66912e1451928
Lorena Mitrea <mitrea.lorenageo@gmail.com>
2019-12-18 10:31:43 +0200

    manage added tutorial version

--- LocalLibrary/wsgi.py
+++ LocalLibrary/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for LocalLibrary project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
+#!/usr/bin/env python
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LocalLibrary.settings')
-
-application = get_wsgi_application()
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locallibrary.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
