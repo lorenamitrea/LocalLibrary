4e8949c6bd134d94c719b6c968aada61b5829f2d
Lorena Mitrea <mitrea.lorenageo@gmail.com>
2019-12-18 10:56:26 +0200

    added allowed_hosts to settings.py

--- locallibrary/wsgi.py
+++ locallibrary/wsgi.py
@@ -1,15 +1,16 @@
-#!/usr/bin/env python
-import os
-import sys
+"""
+WSGI config for LocalLibrary project.
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locallibrary.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locallibrary.settings')
+
+application = get_wsgi_application()
